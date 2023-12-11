9f6bc15bbf0e9212d8623427ed6727099e2575c5
RachelChae <rachel_chae@brown.edu>
2023-12-11 15:41:27 -0500

    pair-prog: fetching database and routing

--- frontend/src/app/levels/page.tsx
+++ frontend/src/app/levels/page.tsx
@@ -5,27 +5,14 @@ import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "aos/dist/aos.css";
 import Script from "next/script";
-
 declare module "*.png";
-// import Slider from "react-slick";
-// // import "~slick-carousel/slick/slick.css";
-// // import "~slick-carousel/slick/slick-theme.css";
 
 interface PageProps {
   children: React.ReactNode;
   progress: number;
 }
 
-// export default function Page({
-//   children,
-//   progress,
-// }: {
-//   progress: number;
-//   children: React.ReactNode;
-// }) {
-//   const selectlevel = "/selectlevel.png";
-//   const level1 = "/level1.png";
-//   const level2 = "/level2.png";
+
 const Page: React.FC<PageProps> = ({ children, progress }) => {
   useEffect(() => {
     // Initialize AOS when the component mounts on the client side
@@ -42,21 +29,6 @@ const Page: React.FC<PageProps> = ({ children, progress }) => {
       <Head>
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
       </Head>
-      {/* <script
-        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossOrigin="anonymous"
-      ></script>
-      <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossOrigin="anonymous"
-      ></script>
-      <script
-        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossOrigin="anonymous"
-      ></script> */}
       <div>
         {children}
         <div className="header">
@@ -70,101 +42,6 @@ const Page: React.FC<PageProps> = ({ children, progress }) => {
           <img className="selectlevel" src={selectlevel}></img>
         </div>
         <div className="container" id="bubbles-container">
-          {/* <Slider></Slider> */}
-          {/* <div
-            id="carouselExampleControls"
-            className="carousel slide"
-            data-ride="carousel"
-          >
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <div className="row">
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                </div>
-                <div className="row" id="row2">
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level1}></img>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="row">
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="row">
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                  <div className="col-sm">
-                    <img className="levelbubble" src={level2}></img>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Next</span>
-            </a>
-          </div> */}
           <div className="overflow-scroll">
             <div className="row" id="row1">
               <div className="col-sm" data-aos="zoom-in">
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,50 @@
+"use client";
+import React, { useEffect, useState } from "react";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+import firebase from "firebase/compat/app";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  setDoc,
+  doc, getDoc
+} from "firebase/firestore";
 
+//import components to pass states into
+import Levels from "./levels/page";
+import Login from "./login/page";
+import Recipes from "./recipes/page";
+import Regions from "./regions/page";
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+
+  //all states for application
+  const [currentUserID, setID] = useState("");
+  const [regions, setRegions] = useState({});
+  const [selectedRegion, setSelectRegion] = useState("");
+
+  const [afriCarib, setAfriCarb] = useState({});
+  const [asian, setAsian] = useState({});
+  const [europe, setEurope] = useState({});
+  const [northAm, setNorthAm] = useState({});
+
+  const afriCaribDoc = doc(firestore, "regions", "AfriCarib");
+
+
+            //   const document = doc(firestore, "users", userId);
+            // const gotDoc = await getDoc(document);
+  return <div>
+
+    <Levels></Levels>
+    <Login></Login>
+    <Recipes></Recipes>
+    <Regions></Regions>
+
+
+  </div>;
 }
