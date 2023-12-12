8d1c1479fd9ecff7b96453906700b396b93ce7ff
Michelle Ding <michelle_ding@brown.edu>
2023-12-12 17:24:34 -0500

    recipes apis

--- frontend/src/app/recipes/page.tsx
+++ frontend/src/app/recipes/page.tsx
@@ -35,53 +35,69 @@ interface recipeProps {
 }
 
 function Page(props : recipeProps) {
-  const selectedString = props.selectedRegion
   const [regionMap, setRegionMap] = useState(new Map());
-  const [userLevel, setUserLevel] = useState(0);
-
-  if (selectedString === "afriCarib"){
-    setRegionMap(props.afriCarib)
-    setUserLevel(props.regions.AfriCarib)
-
-  } else if (selectedString === "asian"){
-    setRegionMap(props.asian)
-    setUserLevel(props.regions.Asia)
-
-  } else if (selectedString === "europe"){
-    setRegionMap(props.europe)
-    setUserLevel(props.regions.Euro)
-
-  } else if (selectedString === "northAm"){
-    setRegionMap(props.northAm)
-    setUserLevel(props.regions.NorthAm)
-
-  }
-
-  const realRecipeList = []
+  const [userLevel, setUserLevel] = useState(1);
+  const [realRecipeList, setRecipeList] = useState([]);
 
   useEffect(() => {
+    // const selectedString = props.selectedRegion
+    console.log("INITIATING RECIPES USER EFFECT LOOP")
+    console.log("user level = ", userLevel)
+    const mockString = "asian";
+    const selectedString = mockString;
+
+    setRegionMap(props.europe)
+    setUserLevel(props.regions.Euro)
+    // if (selectedString === "afriCarib"){
+    //   setRegionMap(props.afriCarib)
+    //   setUserLevel(props.regions.AfriCarib)
+
+    // } else if (selectedString === "asian"){
+    //   setRegionMap(props.asian)
+    //   setUserLevel(props.regions.Asia)
+
+    // } else if (selectedString === "europe"){
+    //   setRegionMap(props.europe)
+    //   setUserLevel(props.regions.Euro)
+
+    // } else if (selectedString === "northAm"){
+    //   setRegionMap(props.northAm)
+    //   setUserLevel(props.regions.NorthAm)
+    // }
+
     if (regionMap[userLevel] !== undefined) { // if user level is not undefined for that region 
       for (const recipe of regionMap[userLevel]) { // looping through string[]
         const document = doc(getFirestore(), "recipes", recipe); // getting the recipe from database
-        const currRecipe = new Map();
+        const currRecipe = new Map<String, any>();
         getDoc(document).then((gotRecipe) => { 
-          console.log(gotRecipe.data().recipe); // name of recipe
-          currRecipe.set("name", gotRecipe.data().recipe)
+          async function mealCall(){
+            try{
+            // console.log(gotRecipe.data().recipe); // name of recipe
+            currRecipe.set("name", gotRecipe.data().recipe)
 
-          console.log(gotRecipe.data().ingredients); // list of ingredients 
-          currRecipe.set("ingredients", gotRecipe.data().ingredients)
+            // console.log(gotRecipe.data().ingredients); // list of ingredients 
+            const ingredientList = gotRecipe.data().ingredients
+            const newIngredientList = ingredientList.filter(function(e){{return (e!="")}})
+            currRecipe.set("ingredients", newIngredientList)
 
-          console.log(gotRecipe.data().instructions); // list of instructions 
-          currRecipe.set("instructions", gotRecipe.data().instructions)
+            // console.log(gotRecipe.data().instructions); // list of instructions 
+            currRecipe.set("instructions", gotRecipe.data().instructions)
+            console.log("calling meal api", currRecipe.get("name"))
 
-          // // api call to mealdb
-          // currRecipe.set("image", gotRecipe.data().instructions)
-          realRecipeList.push(currRecipe)
-        });
-        
+            // populate recipe object with image and cook time 
+            const recipeObj = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + currRecipe.get("name"))
+            const jsonRecipe = await recipeObj.json();
+            currRecipe.set("image", jsonRecipe.meals[0].strMealThumb)
+            }
+            catch (err){
+              console.log("ERROR WITH API:", err)
+          }
+          }
+          mealCall().then(setRecipeList[realRecipeList.push(currRecipe)])
+        })
       }
     }
-  }, [regionMap, userLevel])
+  })
 
   const headerImg = () => {
     const link = "https://lh3.googleusercontent.com/drive-viewer/AK7aPaAmuhZS3kMQMZm5FiPXzabPsXg4tHI3lE7W2mgdheVTjljrr9xVx3mq_yynrnsrLpjA8mKiCTBaM_vz4Niz2DnejcppGA=s1600"
@@ -97,17 +113,17 @@ function Page(props : recipeProps) {
           {"Once you’ve completed it, select the completed button,\
           and the next level will be unlocked!"}
           <br></br>
-          {"Happy cooking!"}</div>
+          {"Happy cooking! ** NOTE: Recipes might take a minute to load. Please be patient!"}</div>
         </div>
     )
   }
 
   const completeButton = () => {
-    const tickimg = "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCt9g6oQ7SqGsX3vlAHM6RprRARgUTtoYh_WytSQEvVFXNZ-k4vDtl-r7fx1OVLZvBVcGxCpgdeOxICYMKXRkjSd_HVug=s2560"
+    const tickimg = "https://lh3.googleusercontent.com/drive-viewer/AK7aPaBAXgyL4y0Rz3PGYb_0J1ADT1RpLLI402AFlicEo77qsknH93IDPHJCmVjTdlhj0vxlOnZpV_eK2EHXsWbywhXnkrc6DA=s2560"
     return(
       <button id="completebutton">
         <div id="buttontext">
-          <img id="tick" src={tickimg}></img>
+          <img id="tick" src={tickimg} referrerPolicy="no-referrer"></img>
           Completed 
         </div>
         </button>
@@ -115,29 +131,32 @@ function Page(props : recipeProps) {
   }
 
   const Recipepage = () => {
-    const recipe1 = {
-      name: "Peking Duck",
-      imagesrc: "https://lh3.googleusercontent.com/drive-viewer/AK7aPaDG-l_kXwjgYHiuPrH3mTx0dlkHD8FoJyhOKnm3yJa6qwVUgdyZ_RSXxZtG3ZDOIMCVcynq-ZebjhvAe66jpP-q6Vx6=s1600",
-      cooktime: "3 hours",
-      ingredients: ["1 tablespoon soy sauce",
-      "2 tablespoons honey, divided",
-      "2 tablespoons, plus 1 teaspoon Chinese five spice, divided",
-      "2 tablespoons kosher salt",
-      "2 teaspoons baking soda",
-      "1 (4-pound) whole air-chilled duck (thawed, if frozen)",
-      "¼ cup hoisin sauce, plus more for serving",
-      "8 boiling water, divided",
-      "For serving: thinly sliced cucumbers, carrots, and scallions, steamed buns or Mandarin pancakes"],
-      instructions: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed doloremque minus repellendus ipsam, ratione perspiciatis esse ducimus modi amet dolor dicta in et voluptatum maxime iusto. Dolores quasi aut numquam!"
-    }
+    console.log("RECIPE PAGE LOADING")
+    console.log("RECIPE PAGE: ", realRecipeList)
+    // ********* MOCK RECIPE *********
+    // const recipe1 = {
+    //   name: "Peking Duck",
+    //   imagesrc: "https://lh3.googleusercontent.com/drive-viewer/AK7aPaDG-l_kXwjgYHiuPrH3mTx0dlkHD8FoJyhOKnm3yJa6qwVUgdyZ_RSXxZtG3ZDOIMCVcynq-ZebjhvAe66jpP-q6Vx6=s1600",
+    //   cooktime: "3 hours",
+    //   ingredients: ["1 tablespoon soy sauce",
+    //   "2 tablespoons honey, divided",
+    //   "2 tablespoons, plus 1 teaspoon Chinese five spice, divided",
+    //   "2 tablespoons kosher salt",
+    //   "2 teaspoons baking soda",
+    //   "1 (4-pound) whole air-chilled duck (thawed, if frozen)",
+    //   "¼ cup hoisin sauce, plus more for serving",
+    //   "8 boiling water, divided",
+    //   "For serving: thinly sliced cucumbers, carrots, and scallions, steamed buns or Mandarin pancakes"],
+    //   instructions: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed doloremque minus repellendus ipsam, ratione perspiciatis esse ducimus modi amet dolor dicta in et voluptatum maxime iusto. Dolores quasi aut numquam!"
+    // }
 
-    const recipelist = [recipe1, recipe1, recipe1]
+    // const recipelist = [recipe1, recipe1, recipe1]
 
     const [imageIndex, setImageIndex] = useState(0);
 
     const settings = {
         className: "center",
-        dots: true,
+        dots: false,
         centerMode: true,
         infinite: true,
         centerPadding: "60px",
@@ -156,35 +175,34 @@ function Page(props : recipeProps) {
     return(
       <div id="recipeslider">
         <Slider {...settings}
-          
         >
           {
             realRecipeList.map((recipe, idx) => 
             <div id="recipecontainer" className={idx===imageIndex? "slide activeSlide": "slide"} key={idx}>
               <div id="rtext">
-                <div id="recipename">{recipe.name}</div>
-                <div id="recipebody"><b>Cook Time:</b> {recipe.cooktime}</div>
+                <div id="recipename">{recipe.get("name")}</div>
+                {/* <div id="recipebody"><b>Cook Time:</b> {recipe.cooktime}</div> */}
                 <div id="recipebody">
                   <b>Ingredients:</b>
-                  {recipe.ingredients.map((item, index) => 
+                  {recipe.get("ingredients").map((item, index) => 
                       (<li key={index}>{item}</li>))}
                 </div>
                 <div id="recipebody">
                   <b>Instructions:</b>
                   <br></br>
-                  {recipe.instructions}</div>
+                  {recipe.get("instructions").map((item, index) => 
+                      (<li key={index}>{item}</li>))}
+                  </div>
               </div>
               <div id="rimgcontainer">
-                <img id="rimg" src={recipe.imagesrc}></img>
+                <img id="rimg" src={recipe.get("image")}></img>
               </div>
-              
             </div>)
           }
         </Slider>
       </div>
     )
   }
-
 
     return (
       <div id="recipepage">
